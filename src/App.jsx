944b4aa021752ba8df525c465c952e1e56cdd8be
Rajat Srivastav <ratneshrsrivastav@gmail.com>
2025-04-19 15:26:53 +0530

    logic update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React,{useState} from 'react'
 
 function App() {
-const [num,setNum]=useState(0)
-function number(event){
-  setNum(event.target.value)
-}
+  const [num, setNum] = useState('')
+
+  function number(curr) {
+    if (curr === 'C') {
+      setNum('')
+    } else if (curr === '+/-') {
+      if (num && num.charAt(0) === '-') setNum(num.slice(1))
+      else setNum('-' + num)
+    } else if (curr === '%') {
+      setNum(String(parseFloat(num) / 100))
+    } else {
+      setNum(num + curr)
+    }
+  }
+
+  function calculate() {
+    const expression = num.replace(/x/g, '*')
+    const result = Number(eval(expression))
+    setNum(result);
+  }
   return(
     <div className='section'>
       <div className='main'>
@@ -14,26 +30,25 @@ function number(event){
           </div>
         </div>
         <div className='keys'>
-          <button onClick={number}>C</button>
-          <button onClick={number}>+/-</button>
-          <button onClick={number}>%</button>
-          <button onClick={number}>/</button>
-          <button onClick={number}>7</button>
-          <button onClick={number}>8</button>
-          <button onClick={number}>9</button>
-          <button onClick={number}>x</button>
-          <button onClick={number}>4</button>
-          <button onClick={number}>5</button>
-          <button onClick={number}>6</button>
-          <button onClick={number}>-</button>
-          <button onClick={number}>1</button>
-          <button onClick={number}>2</button>
-          <button onClick={number}>3</button>
-          <button onClick={number}>+</button>
-          <button onClick={number}>0</button>
-          <button onClick={number}>.</button>
-          <button onClick={number} className='equal'>=</button>
-          
+          <button onClick={()=>{number("C")}}>C</button>
+          <button onClick={()=>{number("+/-")}}>+/-</button>
+          <button onClick={()=>{number("%")}}>%</button>
+          <button onClick={()=>{number("/")}}>/</button>
+          <button onClick={()=>{number(7)}}>7</button>
+          <button onClick={()=>{number(8)}}>8</button>
+          <button onClick={()=>{number(9)}}>9</button>
+          <button onClick={()=>{number("x")}}>x</button>
+          <button onClick={()=>{number(4)}}>4</button>
+          <button onClick={()=>{number(5)}}>5</button>
+          <button onClick={()=>{number(5)}}>6</button>
+          <button onClick={()=>{number("-")}}>-</button>
+          <button onClick={()=>{number(1)}}>1</button>
+          <button onClick={()=>{number(2)}}>2</button>
+          <button onClick={()=>{number(3)}}>3</button>
+          <button onClick={()=>{number('+')}}>+</button>
+          <button onClick={()=>{number(0)}}>0</button>
+          <button onClick={()=>{number(".")}}>.</button>
+          <button onClick={calculate} className='equal'>=</button>
         </div>
       </div>
     </div>
